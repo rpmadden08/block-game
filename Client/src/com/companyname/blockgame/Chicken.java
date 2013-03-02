a01ce668261189eb46f24a246045424ac439d6aa
Ryan Madden <blocky@Ryans-MacBook-Air.local>
2013-03-02 18:44:02 -0500

    things be changn

---
+++ Client/src/com/companyname/blockgame/Chicken.java
@@ -0,0 +1,18 @@
+package com.companyname.blockgame;
+
+import java.awt.*;
+
+public class Chicken extends Mob {
+	public Chicken(int x, int y, int width, int height, int[] id) {
+		super(x, y, width, height, id);
+		
+	}
+	
+	public void tick() {
+		super.tick();
+	}
+	
+	public void render(Graphics g) {
+		super.render(g);
+		}
+}
